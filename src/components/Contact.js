91d247de6fb6605702780aab70e80972a62f80ed
Shahrzad Samimi <shahrzadd@gmail.com>
2022-02-04 20:05:59 -0500

    2nd class added

---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Contact extends React.Component {
+    render() {
+        return(
+        <article>
+    
+        <section id="contact">
+            <h2>Get in Touch</h2>
+            <l>
+                <li><a href="https://www.linkedin.com/in/shahrzad-samimi-118961128">LinkedIn</a></li>
+            </l>
+            <l>
+                <li><a href="https://twitter.com/shahrzadsam">Twitter</a></li>
+            </l>
+            <l>
+                <li><a href="https://www.instagram.com/shahrzad.samimi/">Instagram</a></li>
+            </l>
+        </section>
+
+        </article>
+        )
+    }
+}
+
+export default Contact;
